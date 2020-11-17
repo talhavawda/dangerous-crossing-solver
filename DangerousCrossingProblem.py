a4388d727ebdbc5fc4f4681bd50e3f348c195a0a
talhavawda <talhavawda.dev@gmail.com>
2020-11-18 00:06:23 +0200

    Added Searching Algorithms from search.py

--- DangerousCrossingProblem.py
+++ DangerousCrossingProblem.py
@@ -26,10 +26,10 @@
 
 """
 
-
 import sys
 from collections import deque
-
+import functools
+import heapq
 
 """
 	Helper Functions (from utils.py)
@@ -40,12 +40,33 @@ def is_in(elt, seq):
 	"""Similar to (elt in seq), but compares with 'is', not '=='."""
 	return any(x is elt for x in seq)
 
+
+def memoize(fn, slot=None, maxsize=32):
+	"""Memoize fn: make it remember the computed value for any argument list.
+	If slot is specified, store result in that slot of first argument.
+	If slot is false, use lru_cache for caching the values."""
+	if slot:
+		def memoized_fn(obj, *args):
+			if hasattr(obj, slot):
+				return getattr(obj, slot)
+			else:
+				val = fn(obj, *args)
+				setattr(obj, slot, val)
+				return val
+	else:
+		@functools.lru_cache(maxsize=maxsize)
+		def memoized_fn(*args):
+			return fn(*args)
+
+	return memoized_fn
+
 # =============================================================================
 # =============================================================================
 
 
 """
-	class Problem and Node have been taken from search.py
+	The classes Problem and Node, and the Search Algorithms have been taken from search.py
+	whilst PriorityQueue has been taken from utils.py
 """
 
 
@@ -168,6 +189,167 @@ class Node:
 		# with the same state in a Hash Table
 		return hash(self.state)
 
+# =============================================================================
+
+
+class PriorityQueue:
+	"""A Queue in which the minimum (or maximum) element (as determined by f and
+	order) is returned first.
+	If order is 'min', the item with minimum f(x) is
+	returned first; if order is 'max', then it is the item with maximum f(x).
+	Also supports dict-like lookup."""
+
+	def __init__(self, order='min', f=lambda x: x):
+		self.heap = []
+		if order == 'min':
+			self.f = f
+		elif order == 'max':  # now item with max f(x)
+			self.f = lambda x: -f(x)  # will be popped first
+		else:
+			raise ValueError("Order must be either 'min' or 'max'.")
+
+	def append(self, item):
+		"""Insert item at its correct position."""
+		heapq.heappush(self.heap, (self.f(item), item))
+
+	def extend(self, items):
+		"""Insert each item in items at its correct position."""
+		for item in items:
+			self.append(item)
+
+	def pop(self):
+		"""Pop and return the item (with min or max f(x) value)
+		depending on the order."""
+		if self.heap:
+			return heapq.heappop(self.heap)[1]
+		else:
+			raise Exception('Trying to pop from empty PriorityQueue.')
+
+	def __len__(self):
+		"""Return current capacity of PriorityQueue."""
+		return len(self.heap)
+
+	def __contains__(self, key):
+		"""Return True if the key is in PriorityQueue."""
+		return any([item == key for _, item in self.heap])
+
+	def __getitem__(self, key):
+		"""Returns the first value associated with key in PriorityQueue.
+		Raises KeyError if key is not present."""
+		for value, item in self.heap:
+			if item == key:
+				return value
+		raise KeyError(str(key) + " is not in the priority queue")
+
+	def __delitem__(self, key):
+		"""Delete the first occurrence of key."""
+		try:
+			del self.heap[[item == key for _, item in self.heap].index(True)]
+		except ValueError:
+			raise KeyError(str(key) + " is not in the priority queue")
+		heapq.heapify(self.heap)
+
+# =============================================================================
+# =============================================================================
+
+
+"""
+	Search Algorithms (taken from search.py)
+"""
+
+"""Depth-First Search"""
+
+def depth_first_graph_search(problem):
+	"""
+	[Figure 3.7]
+	Search the deepest nodes in the search tree first.
+	Search through the successors of a problem to find a goal.
+	The argument frontier should be an empty queue.
+	Does not get trapped by loops.
+	If two paths reach a state, only use the first one.
+	"""
+
+	frontier = [(Node(problem.initial))]  # Stack (implemented as a list)
+
+	explored = set()
+	while frontier:
+		node = frontier.pop()
+		if problem.goal_test(node.state):
+			return node
+		explored.add(node.state)
+		frontier.extend(child for child in node.expand(problem)
+						if child.state not in explored and child not in frontier)
+	return None
+
+
+"""Breadth-First Search"""
+
+def breadth_first_graph_search(problem):
+
+
+	node = Node(problem.initial)
+	if problem.goal_test(node.state):
+		return node
+	frontier = deque([node]) #FIFO Queue (implemented as a collection.deque)
+	explored = set()
+	while frontier:
+		node = frontier.popleft()
+		explored.add(node.state)
+		for child in node.expand(problem):
+			if child.state not in explored and child not in frontier:
+				if problem.goal_test(child.state):
+					return child
+				frontier.append(child)
+	return None
+
+
+"""Best-First Search"""
+
+def best_first_graph_search(problem, f, display=False):
+	"""Search the nodes with the lowest f scores first.
+	You specify the function f(node) that you want to minimize; for example,
+	if f is a heuristic estimate to the goal, then we have greedy best
+	first search; if f is node.depth then we have breadth-first search.
+	There is a subtlety: the line "f = memoize(f, 'f')" means that the f
+	values will be cached on the nodes as they are computed. So after doing
+	a best first search you can examine the f values of the path returned.
+	"""
+	f = memoize(f, 'f')
+	node = Node(problem.initial)
+	frontier = PriorityQueue('min', f)
+	frontier.append(node)
+	explored = set()
+	while frontier:
+		node = frontier.pop()
+		if problem.goal_test(node.state):
+			if display:
+				print(len(explored), "paths have been expanded and", len(frontier), "paths remain in the frontier")
+			return node
+		explored.add(node.state)
+		for child in node.expand(problem):
+			if child.state not in explored and child not in frontier:
+				frontier.append(child)
+			elif child in frontier:
+				if f(child) < frontier[child]:
+					del frontier[child]
+					frontier.append(child)
+	return None
+
+
+"""Greedy Best-First Search = Best-First Search with f(n) = h(n)"""
+
+
+"""A-Star Search"""
+
+def astar_search(problem, h=None, display=False):
+	"""A* search is best-first graph search with f(n) = g(n)+h(n).
+	You need to specify the h function when you call astar_search, or
+	else in your Problem subclass.
+	"""
+
+	h = memoize(h or problem.h, 'h')
+	return best_first_graph_search(problem, lambda n: n.path_cost + h(n), display)
+
 
 # ==============================================================================
 # ==============================================================================
@@ -286,7 +468,7 @@ class DangerousCrossing(Problem):
 			Thus the list of actions that is returned is a list of lists (each action is a list)
 
 			:param state: a State in the State Space, a list of integer bits (of size n+1)
-			 				representing the location of the n people and the flashlight
+							representing the location of the n people and the flashlight
 			:return: a list of actions that can be executed/performed on this state
 		"""
 
@@ -490,13 +672,17 @@ def main():
 
 	print()
 	print("test problem")
+
 	p = DangerousCrossing(4,[1,2,5,8],15)
-	actions = p.actions(p.initial)
+	#state = p.initial
+	state = [1,1,1,1,1]
+	actions = p.actions(state)
+	print("State:\t", state)
 
 	for action in actions:
 		print("action:\t\t", action)
 		print("Action cost:\t", p.action_cost(action))
-		print("new state:\t", p.result(p.initial, action))
+		print("new state:\t", p.result(state, action))
 		print()
 
 	#print(p.actions([0,0,1,0,1]))
